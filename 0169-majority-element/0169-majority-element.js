62689f5eb3bb336ed95efa35cbc1162f68318df1
wyoon <dbsdnwns11@naver.com>
2023-07-18 01:20:14 +0900

    Time: 69 ms (54.01%), Space: 45.1 MB (40.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let result = 0, count = 0;
-    for (const num of nums) {
-        if (count == 0) {
-            result = num;
-            count = 1;
-        }
-        else if (result == num) {
-            count++;
-        }
-        else {
-            count--;
+    let dict = {};
+    for (let num of nums) {
+        dict[num] = dict[num] + 1 || 1;
+        if (dict[num] > nums.length / 2) {
+            return (num); 
         }
     }
-    return result;
 };
